e80fb9fcb72f4f99599fcc7fae3e2cf06eda2528
rude <none@none>
2013-09-25 14:11:39 +0200

    Add physfs to main CMakeLists.txt.

--- src/test.cpp
+++ src/test.cpp
@@ -1,14 +1,56 @@
 
+#include <vector>
 #include <iostream>
+#include <sstream>
+#include <functional>
+
 #include <zlib.h>
+#include <physfs.h>
+
+typedef std::stringstream strs;
+typedef std::function<std::string (strs &, strs &)> vfunc;
+
+std::string pad(std::string s, size_t size = 16)
+{
+	while (s.length() < size)
+		s.append(" ");
+
+	return s;
+}
 
 int main(int argc, const char **argv)
 {
-	const char *prefix = " -- ";
+	vfunc zlib = [](strs &c, strs &l)
+	{
+		c << ZLIB_VERSION;
+		l << zlibVersion();
+		return "zlib";
+	};
 
-	std::cout << prefix << "Starting test" << std::endl;
-	std::cout << prefix << "zlib version: " << zlibVersion() << std::endl;
-	std::cout << prefix << "Ending test" << std::endl;
+	vfunc physfs = [](strs &c, strs &l)
+	{
+		PHYSFS_Version compiled;
+		PHYSFS_Version linked;
 
-	return 0;
+		PHYSFS_VERSION(&compiled);
+		PHYSFS_getLinkedVersion(&linked);
+
+		c << (int)compiled.major << "." << (int)compiled.minor << "." << (int)compiled.patch;
+		l << (int)linked.major << "." << (int)linked.minor << "." << (int)linked.patch;
+		return "PhysicsFS";
+	};
+
+	std::vector<vfunc> funcs;
+	funcs.push_back(zlib);
+	funcs.push_back(physfs);
+
+	for (size_t i = 0; i < funcs.size(); ++i)
+	{
+		vfunc f = funcs[i];
+		strs c, l;
+		std::string name = f(c, l);
+		std::cout << "-- " << pad(name) << "   compiled: " << pad(c.str(), 7) << "   linked: " << pad(l.str(), 7) << std::endl;
+	}
+
+	return getchar();
 }
