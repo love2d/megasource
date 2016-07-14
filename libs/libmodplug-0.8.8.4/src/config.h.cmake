5bb2a2facbfd89a2b8d7dae1c6f0df3b27b476cd
rude <andruud@gmail.com>
2016-07-14 16:27:59 +0200

    Make stuff buildable on Mac.

---
+++ libs/libmodplug-0.8.8.4/src/config.h.cmake
@@ -0,0 +1,13 @@
+#cmakedefine HAVE_SINF
+#cmakedefine HAVE_SETENV
+#cmakedefine HAVE_DLFCN_H
+#cmakedefine HAVE_INTTYPES_H
+#cmakedefine HAVE_MALLOC_H
+#cmakedefine HAVE_MEMORY_H
+#cmakedefine HAVE_STDINT_H
+#cmakedefine HAVE_STDLIB_H
+#cmakedefine HAVE_STRINGS_H
+#cmakedefine HAVE_STRING_H
+#cmakedefine HAVE_SYS_STAT_H
+#cmakedefine HAVE_SYS_TYPES_H
+#cmakedefine HAVE_UNISTD_H
